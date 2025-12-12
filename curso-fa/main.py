37854c5fe6fe4ef0d49688a48a1fd810df271034
dr1602 <david.arangelg@gmail.com>
2025-12-12 13:14:40 -0600

    feat: adds different methods to send the hour

--- curso-fa/main.py
+++ curso-fa/main.py
@@ -1,3 +1,5 @@
+import zoneinfo
+from datetime import datetime
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,6 +8,26 @@ app = FastAPI()
 async def root():
     return {"message": "Hola, Dave!"}
 
-# Para correr ejecuta: fastapi dev
-# api: http://localhost:8000/
-# swagger: http://localhost:8000/docs
+country_timezones = {
+    "CO": "America/Bogota",
+    "MX": "America/Mexico_City",
+    "AR": "America/Argentina/Buenos_Aires",
+    "BR": "America/Sao_Paulo",
+    "PE": "America/Lima",
+}
+
+@app.get("/time")
+async def specific_time():
+    return {"time": datetime.now()}
+
+@app.get("/time/{iso_code}")
+async def multiple_times(iso_code: str):
+    iso = iso_code.upper()
+    timezone_str = country_timezones.get(iso)
+    tz = zoneinfo.ZoneInfo(timezone_str)
+    return {"time": datetime.now(tz)}
+
+# Nuevo reto: crear nuevo reto de endpoint que recia una variable en formato get 
+# y que automaticamente, pueda habilitar el formato de hora, 
+# como devolver la hora en un formato de 24 hrs
+
