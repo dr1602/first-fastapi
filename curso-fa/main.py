2d3c952fa3a5562f63d27ad26fad28472a860a4d
dr1602 <david.arangelg@gmail.com>
2025-12-12 17:58:23 -0600

    feat: adds a dynamic endpoint to accept and return a formatted date

--- curso-fa/main.py
+++ curso-fa/main.py
@@ -27,6 +27,23 @@ async def multiple_times(iso_code: str):
     tz = zoneinfo.ZoneInfo(timezone_str)
     return {"time": datetime.now(tz)}
 
+@app.get("/time/format/{format_code}")
+async def formatted_time(format_code: str):
+    format = format_code.lower()
+    date = datetime.now()
+    if format == 'iso':
+        return { date.isoformat() }
+    elif format == 'slash':
+        return { date.strftime("%d/%m/%y") }
+    elif format == 'written':
+        return { date.strftime("%A %d. %B %Y") }
+    elif format == 'written':
+        return { date.ctime() }
+    elif format == 'special':
+        return { 'The {1} is {0:%d}, the {2} is {0:%B}.'.format(date, "day", "month") }
+    else:
+        return {"time without format": datetime.now()}
+
 # Nuevo reto: crear nuevo reto de endpoint que recia una variable en formato get 
 # y que automaticamente, pueda habilitar el formato de hora, 
 # como devolver la hora en un formato de 24 hrs
